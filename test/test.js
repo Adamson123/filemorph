24f4eef5af3cb25a400b920cff248240dd12cadc
Ajibade Adam <dapoajibade66@gmail.com>
2024-12-18 07:36:46 +0100

    Remove test.js file, which contained a script for executing the filemorph CLI command and writing output to a file.

--- test/test.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from "child_process";
-import fs from "fs";
-import stripAnsi from "strip-ansi";
-
-const output = execSync("filemorph list --r true", { encoding: "utf8" });
-fs.writeFileSync("output", stripAnsi(output.toString()));
-
-console.log(output);
